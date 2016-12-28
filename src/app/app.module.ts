88884a8a77b1cab516e7a3533bef6a855f1b0b2e
Bat3744 <baptiste.bordet@gmail.com>
2016-12-28 22:50:22 +0100

    fix css 10

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,11 @@ import { NgModule } from '@angular/core'
 import { RouterModule } from '@angular/router';
 import { rootRouterConfig } from './app.routes';
 import { AppComponent } from './app.component';
-import { GithubService } from './github/shared/github.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 
-import { AboutComponent } from './about/about.component';
-import { HomeComponent } from './home/home.component';
-import { RepoBrowserComponent } from './github/repo-browser/repo-browser.component';
-import { RepoListComponent } from './github/repo-list/repo-list.component';
-import { RepoDetailComponent } from './github/repo-detail/repo-detail.component';
 import { LocationStrategy, HashLocationStrategy } from '@angular/common';
-import { ContactComponent } from './contact/contact.component';
 
 import { HeaderComponent }   from './header/header.component';
 import { ValeursComponent } from './valeurs/valeurs.component';
@@ -22,12 +15,6 @@ import { PrestationsComponent } from './prestations/prestations.component';
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent,
-    RepoBrowserComponent,
-    RepoListComponent,
-    RepoDetailComponent,
-    HomeComponent,
-    ContactComponent,
     HeaderComponent,
     ValeursComponent,
     PrestationsComponent
@@ -38,9 +25,6 @@ import { PrestationsComponent } from './prestations/prestations.component';
     ReactiveFormsModule,
     HttpModule,
     RouterModule.forRoot(rootRouterConfig, { useHash: true })
-  ],
-  providers: [
-    GithubService
   ],
   bootstrap: [ AppComponent ]
 })
