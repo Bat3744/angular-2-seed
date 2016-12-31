f73fb59cc5f3fdd35ecacdca123764c5158aa511
Bat3744 <baptiste.bordet@gmail.com>
2016-12-31 13:35:10 +0100

    contact component

---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { Router }            from '@angular/router';
+import { Observable }        from 'rxjs/Observable';
+import { Subject }           from 'rxjs/Subject';
+
+@Component({
+  //moduleId: module.id,
+  selector:    'contact',
+  templateUrl: 'contact.component.html'
+})
+
+export class ContactComponent {
+
+
+}
