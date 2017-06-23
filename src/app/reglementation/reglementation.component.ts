908428af447c1301714c79093f789d70fc6d1dbe
Baptiste Bordet <baptiste.bordet.soat@ext.pmu.fr>
2017-06-23 12:33:56 +0200

    Ajout bloc reglementation

--- src/app/reglementation/reglementation.component.ts
+++ src/app/reglementation/reglementation.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit }  from '@angular/core';
-import { Router }                   from '@angular/router';
 
 @Component({
   //moduleId: module.id,
@@ -9,7 +8,6 @@ import { Router }                   from '@angular/router';
 
 export class ReglementationComponent {
 
-  srcImg1 = require('../../static/img1-2.png');
-  srcImg2 = require('../../static/img2-2.png');
+  reglementationSrc = require('../../static/reglementation_1.png');
 
 }
