0f8df3485bbb7c06ca27deb0e83b9384dd0894e6
Baptiste Bordet <baptiste.bordet.soat@ext.pmu.fr>
2017-09-11 13:29:37 +0200

    Ajout mentions legales

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import * as $ from 'jquery';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
 	selector:    'footer',
 	templateUrl: 'footer.component.html'
 })
 
-export class FooterComponent {
+export class FooterComponent implements AfterViewInit {
+
+	ngAfterViewInit() {
+		$('#mentionsLegalesCollapse').on('show.bs.collapse', function () {
+			$('html,body').animate({scrollTop: document.body.scrollHeight}, 'slow');
+		});
+	}
 
 }
