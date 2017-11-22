75dc8ff0f9fad9f5912c2e9471f06108cc639a4d
Baptiste Bordet <baptiste.bordet.soat@ext.pmu.fr>
2017-11-22 16:58:52 +0100

    clean server

--- server/app.ts
+++ server/app.ts
@@ -3,27 +3,7 @@ import * as bodyParser from 'body-parser';
 import { FormValidator } from '../server/formValidator';
 import { MailUtils } from '../server/mailUtils';
 
-const app = express(),
-	env = process.env.NODE_ENV || 'development',
-	forceSsl = function (req, res, next) {
-		if (req.headers['x-forwarded-proto'] !== 'https') {
-			return res.redirect(['https://', req.get('Host'), req.url].join(''));
-		}
-		return next();
-	};
-
-if (env === 'production') {
-	app.use(forceSsl);
-}
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-
-app.use(function (req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	next();
-});
+const app = express();
 
 app.get('/api/test', function (req, res) {
 	res.send('Hello test World!');
