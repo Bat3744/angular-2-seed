e145e1f385de582ee40d0b5b42f2eebbd5916446
Baptiste Bordet <baptiste.bordet.soat@ext.pmu.fr>
2017-11-27 13:49:22 +0100

    add static file to expres 8

--- server/app.ts
+++ server/app.ts
@@ -7,8 +7,10 @@ import * as path from 'path';
 
 const app = express();
 
+app.use('/', express.static(__dirname));
+
 app.get('/', function(req, res) {
-	res.sendFile(path.join(__dirname + '/../index.html'));
+	res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.get('/api/test', function (req, res) {
